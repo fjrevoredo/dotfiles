216de18dc3df111bd66be8ea7470131e7cf71e42
Francisco J. Revoredo <fjrevoredo@gmail.com>
2026-04-30 14:31:25 +0200

    add new tab handling shortcuts

--- .wezterm.lua
+++ .wezterm.lua
@@ -211,6 +211,21 @@ config.keys = {
     mods = 'LEADER',
     action = act.CloseCurrentPane { confirm = true },
   },
+
+  -- TAB NAVIGATION (Leader + number)
+  { key = '1', mods = 'LEADER', action = act.ActivateTab(0) },
+  { key = '2', mods = 'LEADER', action = act.ActivateTab(1) },
+  { key = '3', mods = 'LEADER', action = act.ActivateTab(2) },
+  { key = '4', mods = 'LEADER', action = act.ActivateTab(3) },
+  { key = '5', mods = 'LEADER', action = act.ActivateTab(4) },
+  { key = '6', mods = 'LEADER', action = act.ActivateTab(5) },
+  { key = '7', mods = 'LEADER', action = act.ActivateTab(6) },
+  { key = '8', mods = 'LEADER', action = act.ActivateTab(7) },
+  { key = '9', mods = 'LEADER', action = act.ActivateTab(8) },
+
+  -- TAB REORDER (Ctrl+Shift+Arrow)
+  { key = 'LeftArrow',  mods = 'CTRL|SHIFT', action = act.MoveTabRelative(-1) },
+  { key = 'RightArrow', mods = 'CTRL|SHIFT', action = act.MoveTabRelative(1) },
 }
 
 return config
